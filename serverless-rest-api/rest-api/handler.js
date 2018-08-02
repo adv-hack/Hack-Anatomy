12cac4e4a2578dcc776f1f51d0d6ec60faecb7ef
Jaimin-Patel30 <31537143+Jaimin-Patel30@users.noreply.github.com>
2018-08-03 00:50:11 +0530

    Update handler.js

--- serverless-rest-api/rest-api/handler.js
+++ serverless-rest-api/rest-api/handler.js
@@ -198,7 +198,9 @@ module.exports.Result = (event, context, callback) => {
                   body: JSON.stringify(answers),
                   headers: {
                   'Accept': 'application/json',
-                  'Content-Type': 'application/json'
+                  'Content-Type': 'application/json',
+                  'Access-Control-Allow-Origin': '*',
+                   'Access-Control-Allow-Credentials': true,
                   }
                 })
                 .then(() => {
