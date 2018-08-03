0986eaf6df70e34862747987671e929a3c36129b
Jaimin-Patel30 <31537143+Jaimin-Patel30@users.noreply.github.com>
2018-08-03 14:53:14 +0530

    Update hello.py

--- AWS-practise/pythonDeployment/hello.py
+++ AWS-practise/pythonDeployment/hello.py
@@ -46,7 +46,7 @@ def sen_sim(sen):
         absvalue=abs(model_sentiment-act_sentiment)
         if absvalue==0:
             absvalue=1
-        sentimentvalue=2/absvalue
+        sentimentvalue=absvalue/2
         sim1=lib.run_avg_benchmark(modelAns,actAns,model=lib.word2vec)
         sim2=lib.word_vectors.wmdistance(modelAns,actAns)
         sim2=(8-sim2)/8
