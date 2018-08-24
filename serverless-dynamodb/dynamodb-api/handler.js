07795c934392ee2a1cc4c9c2ba2ecabf941bb263
Jaimin Patel <jp30101995@gmail.com>
2018-08-24 08:42:34 +0530

    Adding Dynamo

---
+++ serverless-dynamodb/dynamodb-api/handler.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.hello = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.createQue = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  console.log(event.body);
+  const data = JSON.parse(event);
+  if (typeof data.text !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the todo item.',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: question-dev,
+    Item: {
+      id: uuid.v1(),
+      question: data.text
+      // questiontype: data.questiontype,
+      // difficulty: data.difficulty,
+      // options: data.options,
+      // subject: data.subject,
+      // answer: data.answer
+    },
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the todo item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
