337ec28db6c4b80ace0c2f147c1d3015feed80af
Jaimin-Patel30 <31537143+Jaimin-Patel30@users.noreply.github.com>
2018-09-08 14:37:57 +0530

    Update hello.py

--- AWS-practise/pythonDeployment/hello.py
+++ AWS-practise/pythonDeployment/hello.py
@@ -16,6 +16,15 @@ CORS(app)
 def hello():
     return "Hello World2!"
 
+@app.route('/recommend/<nm>',methods = ['POST', 'GET'])
+def recommend(nm):
+    if request.method == 'POST':
+        content = request.get_json(force=True)
+        return lib.getRecommendation(content["model"], content["EasyQuestions"], content["MediumQue"], content["HardQuestions"], content["AvgPerEasyQue"], content["AvgPerMedQue"], content["AvgPerHardQue"])
+    else:
+        return ''
+
+
 @app.route('/predict/<nm>',methods = ['POST', 'GET'])
 def login(nm):
     if request.method == 'POST':
